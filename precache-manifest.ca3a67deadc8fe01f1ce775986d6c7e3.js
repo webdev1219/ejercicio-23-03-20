e33aaea451893af4e720a62e8a60e6e82a708ef7
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2020-04-23 16:14:12 +0200

    update

---
+++ precache-manifest.ca3a67deadc8fe01f1ce775986d6c7e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c467d09f3f8b7622586b0b13c0cd2d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86ace6ab1bb90923d410",
+    "url": "/static/css/main.de6a2965.chunk.css"
+  },
+  {
+    "revision": "245ae5fd75cb58b9abcf",
+    "url": "/static/js/2.99f93540.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/static/js/2.99f93540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86ace6ab1bb90923d410",
+    "url": "/static/js/main.704e73db.chunk.js"
+  },
+  {
+    "revision": "db08be017f9cbeb5ecbf",
+    "url": "/static/js/runtime-main.1e21de0b.js"
+  }
+]);
